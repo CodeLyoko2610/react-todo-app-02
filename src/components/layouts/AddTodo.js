5b0299ee05723505ec15be5ae8f43d180d03f837
chuongcuu <dangchuong1999@outlook.com>
2020-01-15 01:41:20 +0200

    Component level state in AddTodo | Add property value  and onChange handler to capture user input.

--- src/components/layouts/AddTodo.js
+++ src/components/layouts/AddTodo.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export default class AddTodo extends React.Component {
+  state = {
+    title: ''
+  }; //component level state - only live inside this component
+
+  onChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
   render() {
     return (
       <form style={{ display: 'flex' }}>
@@ -9,6 +19,8 @@ export default class AddTodo extends React.Component {
           name='title'
           placeholder='Add a new Todo...'
           style={{ flex: '10', padding: '10px' }}
+          value={this.state.title}
+          onChange={this.onChange}
         />
         <input
           type='submit'
